4eea1391bf08ed30595f5bae64a24ba80eddd470
Kaushik Baruah <96105428+K4350@users.noreply.github.com>
2022-09-04 23:33:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ else if(randomNumber2>randomNumber1){
 }
 else{
     document.querySelector("h1").innerHTML = " Draw";
-<<<<<<< HEAD
 }
-
 function refreshPage() {
     window.location.reload();
-}
+} 
